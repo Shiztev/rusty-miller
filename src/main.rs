f1a9778051eb4ec92fb12ba86c618b8dd8f7dafc
Stevie Alvarez <stevie.6strings@gmail.com>
2022-12-13 16:54:33 -0500

    added globals

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod prime;
 
+#[macro_use]
+extern crate lazy_static;
+extern crate rand;
+
 fn main() {
     println!("Hello, world!");
 }
--- src/prime/mod.rs
+++ src/prime/mod.rs
@@ -1,24 +1,51 @@
-use num::{BigUint, BigInt};
+use num::{BigUint, BigInt, bigint::RandBigInt};
+use rand::rngs::ThreadRng;
+
+/*thread_local! {
+  pub static RNG: ThreadRng = rand::thread_rng();
+}*/
+
+lazy_static!(
+  pub static ref BZERO: BigUint = create_biguint(0);
+  pub static ref BONE: BigUint = create_biguint(1);
+  pub static ref BTWO: BigUint = create_biguint(2);
+);
+pub static ZERO: u32 = 0;
+pub static ONE: u32 = 1;
+pub static TWO: u32 = 2;
 
 
-fn create_biguint(i: i32) -> BigUint {
-  match BigInt::to_biguint(&BigInt::from(i)) {
-    Some(v) => return v,
-    None => panic!("Cannot convert {i} to BigUint"),
-  }
+
+/// Create a BigUint from the provided i32.
+fn create_biguint(i: u32) -> BigUint {
+  BigUint::from(i)
 }
+
 
 /// Calculates if provided number is probabilistically prime
 /// using the Miller-Rabin primality test.
-pub fn miller_rabin(value: BigUint, k: u32) -> bool {
-  let r: BigUint;
-  let d: BigUint;
-  let a: BigUint;
+pub fn miller_rabin(value: BigUint, k: u32, rng: &mut ThreadRng) -> bool {
+  let mut r: BigUint;
+  let mut d: BigUint;
+  let mut a: BigUint;
   let x: BigUint;
   let cont: bool = false;
 
-  if (value > create_biguint(3)) && (value.modpow(&create_biguint(1), &create_biguint(2)) != create_biguint(0)) {
+  if (value > create_biguint(3)) && (value.modpow(&BONE, &BTWO) != *BZERO) {
+    r = (*BZERO).clone();
+    d = &value - ONE;
+    loop {
+      d /= TWO;
+      r += ONE;
+      if d.modpow(&BONE, &BTWO) != *BZERO { break; }
+    }
 
+    for _ in 0..k {
+      loop {
+        a = rng.gen_biguint(value.bits() / 8);
+        if !(a < *BTWO || a > (value - TWO)) { break; }
+      }
+    }
   }
 
   false
