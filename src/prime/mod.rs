aa1be2147e3679c75e24cc6abb058f6b12143acd
Stevie Alvarez <stevie.6strings@gmail.com>
2022-12-16 14:48:17 -0500

    clean up unused globals and fix some styling issues

--- src/prime/mod.rs
+++ src/prime/mod.rs
@@ -1,16 +1,11 @@
-use num::{BigUint, BigInt, bigint::RandBigInt};
+use num::{BigUint, bigint::RandBigInt};
 use rand::rngs::ThreadRng;
-
-/*thread_local! {
-  pub static RNG: ThreadRng = rand::thread_rng();
-}*/
 
 lazy_static!(
   pub static ref BZERO: BigUint = create_biguint(0);
   pub static ref BONE: BigUint = create_biguint(1);
   pub static ref BTWO: BigUint = create_biguint(2);
 );
-pub static ZERO: u32 = 0;
 pub static ONE: u32 = 1;
 pub static TWO: u32 = 2;
 
@@ -63,7 +58,6 @@ pub fn miller_rabin(value: BigUint, k: u32, rng: &mut ThreadRng) -> bool {
       if cont {
         continue;
       }
-
       return false;
     }
 
